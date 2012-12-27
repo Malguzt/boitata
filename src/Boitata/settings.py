1e567dbb51d8d4e561df02026b6526fe6a1350e2
Lenscak, Jose Francisco <jose.lenscak@globant.com>
2012-12-27 16:50:37 -0300

    Change sql configuration.

--- src/Boitata/settings.py
+++ src/Boitata/settings.py
@@ -2,7 +2,7 @@
 # Django settings for Boitata project.
 import os
 
-PROYECT_ROOT = os.path.dirname(os.path.realpath(__file__))
+PROJECT_ROOT = os.path.dirname(os.path.realpath(__file__))
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -14,11 +14,11 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'boitata',                      # Or path to database file if using sqlite3.
-        'USER': 'boitata',                      # Not used with sqlite3.
-        'PASSWORD': 'YUAN1E8q',                  # Not used with sqlite3.
-        'HOST': '192.168.1.101',                      # Set to empty string for localhost. Not used with sqlite3.
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': os.path.join(PROJECT_ROOT,'sql/sqlite.db'),                      # Or path to database file if using sqlite3.
+        'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
