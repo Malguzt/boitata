c189bd224f5d747f5489b093b26549b5367862a5
Lenscak José Francisco <malguzt@gmail.com>
2013-02-26 01:11:18 -0300

    Add new_user view.

--- src/player/views.py
+++ src/player/views.py
@@ -1,1 +1,17 @@
-# Create your views here.
+from django.contrib.auth.forms import UserChangeForm, UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect
+from django.shortcuts import render_to_response
+from django.template.context import RequestContext
+
+def new_user(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid:
+            form.save()
+            return HttpResponseRedirect('/')
+        else:
+            form = UserCreationForm()
+        return render_to_response('new_user.html', {'form':form}, context_instance=RequestContext(request))
